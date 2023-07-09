08cc4b1667474cf7663fc00e791aa6fc95e653f9
aabdeen <ahmed.yahya18@gmail.com>
2023-07-09 10:38:43 +0300

    Refactor + Code Cleaning

--- webprogram/main.go
+++ webprogram/main.go
@@ -8,6 +8,10 @@ import (
 	"net/http"
 )
 
+type Art struct {
+	Output string
+}
+
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
@@ -18,27 +22,17 @@ func main() {
 	}
 }
 
-type Testing struct {
-	Title string
-	News  string
-}
-
 func ArtHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
 	}
-	InputString := r.FormValue("input")
+	inputString := r.FormValue("input")
 	banner := r.FormValue("banner")
-	InputString = asciiart.AsciiArt(InputString, banner)
-	p := Testing{Title: InputString, News: "testing"}
-	// fmt.Println(p)
-	// testing := "test"
-	// fmt.Fprintf(w, "%s", InputString)
-	// fmt.Println("")
+	output := Art{Output: asciiart.AsciiArt(inputString, banner)}
 	t, err := template.ParseFiles("template.html")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Print(t.Execute(w, p))
+	t.Execute(w, output)
 }
